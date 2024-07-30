741ffc32be5237c89f667bf21a62c5cefcec9240
yart <jarocki.art@gmail.com>
2024-07-30 20:40:36 +0300

    feat(lesson-1): improved CUI.input

--- lib/cui/input.rb
+++ lib/cui/input.rb
@@ -3,8 +3,11 @@
 module CUI
   # @param prompt [String] will be shown to user
   # @return [String, Integer] according ot user input
-  def input(prompt)
-    print "#{prompt} > "
+  def input(prompt = nil)
+    prompt = "#{prompt} " unless prompt.nil?
+
+    print "#{prompt}> "
+
     string = gets.chomp
 
     string == string.to_i.to_s ? string.to_i : string
