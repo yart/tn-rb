9aee038bff359ad9287a1bc1ccd2d1c3ad1cc8cd
yart <jarocki.art@gmail.com>
2024-08-03 20:21:07 +0300

    refactor(cui): Moved Number to a separate file

---
+++ lib/cui/input/number.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module CUI
+  # Provides a simple input tools for getting data from users.
+  module Input
+    # Recceives any numbers from user.
+    class Number < Text
+      include Numerable
+
+      # @param [Hash] settings
+      # @option settings [String] :label an explanation for prompt
+      # @option settings [String] :prompt '> " by default
+      # @option settings [Boolean] :allow_negatives when input type is Integer, false by default
+      def initialize(**)
+        super(**)
+        setup_negatives(**)
+      end
+    end
+  end
+end
