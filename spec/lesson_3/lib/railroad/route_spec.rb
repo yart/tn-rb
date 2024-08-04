a5cccec18b7e8ea3053dec8760c53d1b50a9bc59
yart <jarocki.art@gmail.com>
2024-08-05 02:27:52 +0300

    test(lesson-3): Added spec for Railroad::Route

---
+++ spec/lesson_3/lib/railroad/route_spec.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+class DummyStation
+  attr_reader :name
+
+  def initialize(name) = @name = name
+end
+
+describe Railroad::Route do
+  let!(:route) { described_class.new(DummyStation.new('first'), DummyStation.new('last')) }
+
+  context 'when no stations passed to a new route' do
+    it { expect { described_class.new }.to raise_error(/No stations/) }
+  end
+
+  context 'when created has first and last stations' do
+    it { expect(route).to respond_to(:list) }
+    it { expect(route).to respond_to(:add).with(1).argument }
+    it { expect(route).to respond_to(:remove).with(1).argument }
+
+    describe '#list' do
+      it { expect(route.list).to eq %w[first last] }
+    end
+  end
+
+  context 'when add or remove stations' do
+    let(:other) { DummyStation.new('other') }
+
+    before { route.add(other) }
+
+    describe '#add' do
+      it { expect(route.list.count).to eq 3 }
+      it { expect(route.list[1]).to eq 'other' }
+    end
+
+    describe '#remove' do
+      before { route.remove(other) }
+
+      it { expect(route.list.count).to eq 2 }
+      it { expect(route.list.first).to eq 'first' }
+      it { expect(route.list.last).to eq 'last' }
+    end
+  end
+end
