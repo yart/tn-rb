1ea00fce51f48e3064ae43c99293bd27aa8db6e7
yart <jarocki.art@gmail.com>
2024-08-12 05:44:49 +0300

    feat(cui): module CUI changed according to the last changes

--- lib/cui.rb
+++ lib/cui.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
 require 'io/console'
+require 'timeout'
 
 # Simple console user interface tools.
 module CUI; end
 
+require_relative 'cui/constants'
+require_relative 'cui/control'
+require_relative 'cui/view'
+require_relative 'cui/dialog'
 require_relative 'cui/input'
+require_relative 'cui/list'
+require_relative 'cui/multiselect'
---
+++ lib/cui/multiselect.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module CUI
+  class Multiselect; end
+end
