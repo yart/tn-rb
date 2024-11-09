7f5512b7bf345d02fc6bc351f8f1de1a089a09bc
yart <jarocki.art@gmail.com>
2024-11-09 08:39:41 +0300

    fix(lesson_4): fixed path for View::BaseMenu in requirements of view.rb

--- lesson_4/app/view.rb
+++ lesson_4/app/view.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require_relative 'view/dashboard'
-require_relative 'view/menu'
+require_relative 'view/base_menu'
 require_relative 'view/routes'
 require_relative 'view/stations'
 require_relative 'view/trains'
