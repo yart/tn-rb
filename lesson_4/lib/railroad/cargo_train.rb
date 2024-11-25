1234f87aca4253fa20a2d31e92518377b274a6af
yart <jarocki.art@gmail.com>
2024-11-25 14:34:02 +0300

    docs(lesson_4): added for Railroad::CargoTrain and Railroad::PassengerTrain

--- lesson_4/lib/railroad/cargo_train.rb
+++ lesson_4/lib/railroad/cargo_train.rb
@@ -2,9 +2,26 @@
 
 module Lesson4
   module Railroad
-    # Manages trains.
+    # Represents a cargo train.
+    #
+    # This class specializes `Lesson4::Railroad::Train` for managing cargo trains.
+    # It sets the wagon type to `Lesson4::Railroad::CargoWagon` by default.
+    #
+    # @example Creating a cargo train
+    #   cargo_train = Lesson4::Railroad::CargoTrain.new(number: 'C123')
+    #
+    # @example Attaching a wagon
+    #   wagon = Lesson4::Railroad::CargoWagon.new
+    #   cargo_train.attach(wagon)
+    #
+    # @example Moving along a route
+    #   cargo_train.route = some_route
+    #   cargo_train.forward
+    #   cargo_train.location(:current) # => current station on the route
     class CargoTrain < Train
-      # @param number [String]
+      # Initializes a cargo train.
+      #
+      # @param number [String] The unique identifier for the train.
       def initialize(number:)
         super(number:)
 
--- lesson_4/lib/railroad/passenger_train.rb
+++ lesson_4/lib/railroad/passenger_train.rb
@@ -2,9 +2,26 @@
 
 module Lesson4
   module Railroad
-    # Manages trains.
+    # Represents a passenger train.
+    #
+    # This class specializes `Lesson4::Railroad::Train` for managing passenger trains.
+    # It sets the wagon type to `Lesson4::Railroad::PassengerWagon` by default.
+    #
+    # @example Creating a passenger train
+    #   passenger_train = Lesson4::Railroad::PassengerTrain.new(number: 'P456')
+    #
+    # @example Attaching a wagon
+    #   wagon = Lesson4::Railroad::PassengerWagon.new
+    #   passenger_train.attach(wagon)
+    #
+    # @example Moving along a route
+    #   passenger_train.route = some_route
+    #   passenger_train.backward
+    #   passenger_train.location(:previous) # => previous station on the route
     class PassengerTrain < Train
-      # @param number [String]
+      # Initializes a passenger train.
+      #
+      # @param number [String] The unique identifier for the train.
       def initialize(number:)
         super(number:)
 
