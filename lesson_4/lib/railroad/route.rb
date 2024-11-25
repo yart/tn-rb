ba88550ccc1ffb1127ad3c81405c1af130ebc374
yart <jarocki.art@gmail.com>
2024-11-25 11:11:59 +0300

    refactor(lesson_4): updates in Railroad::Route
    - improved commentaries to the class Railroad::Route
    - excluded unused method #stations_on_start
    - improved .new and StationsNumberError.new to be more readable
    - added RBS for this class
    - improved some definitions in the test for better understanding the class possibilities, limitations and so on

--- lesson_4/lib/railroad/route.rb
+++ lesson_4/lib/railroad/route.rb
@@ -2,38 +2,76 @@
 
 module Lesson4
   module Railroad
-    # Manages routes.
+    # Represents a route between two or more stations.
+    #
+    # A route always starts and ends with two mandatory stations: the first and the last.
+    # Additional stations can be added or removed, except for the first and last stations.
+    #
+    # This class operates on objects that respond to `#name`. The `#name` method should return
+    # a value that can be converted to a string, but it is not strictly required to return a `String`.
+    #
+    # @example Creating a route
+    #   first_station = OpenStruct.new(name: 'First')
+    #   last_station = OpenStruct.new(name: 'Last')
+    #   route = Lesson4::Railroad::Route.new(first_station, last_station)
+    #
+    # @example Adding and removing stations
+    #   other_station = OpenStruct.new(name: 'Other')
+    #   route.add(other_station)    # Adds an intermediate station
+    #   route.remove(other_station) # Removes the intermediate station
+    #
     class Route
+      # Number of stations required to initialize a route.
       STATIONS_ON_START = 2
 
-      # When wrong number of arguments was passed to .new
+      # Error raised when the wrong number of stations is provided during initialization.
       class StationsNumberError < ::StandardError
-        # @param number [Integer]
+        # Initializes the error message.
+        #
+        # @param number [Integer] The number of stations provided.
         def initialize(number)
-          message_begin = number.zero? ? 'No stations' : 'Only one station'
-          message_begin = "#{number} stations" if number > STATIONS_ON_START
-          super "#{message_begin} station given, must be only two!"
+          message_begin =
+            case number
+            when 0 then 'No stations'
+            when 1 then 'Only one station'
+            else "#{number} stations"
+            end
+          super "#{message_begin} given, must be only two!"
         end
       end
 
-      # @param list [Array<Railroad::Station>]
-      def initialize(*list) = list.size != STATIONS_ON_START ? raise(StationsNumberError, list.size) : @list = list
+      # Initializes a new route with exactly two stations.
+      #
+      # @param list [Array<#name>] Two objects that respond to `#name`.
+      # @raise [StationsNumberError] if the number of stations provided is not exactly two.
+      def initialize(*list)
+        raise StationsNumberError, list.size unless list.size == STATIONS_ON_START
 
-      # @return [Array<String>]
-      def list              = @list.map(&:name)
-      # @param station [Railroad::Station]
-      # @return [Array<Railroad::Station>]
-      def add(station)      = @list.insert(-2, station)
-
-      # @param station [Railroad::Station]
-      # @return [Array<Railroad::Station>, nil]
-      def remove(station)
-        @list.delete(station) if station != @list.first || station != @list.last
+        @list = list
       end
 
-      private
+      # Returns the list of station names in the route.
+      #
+      # @return [Array<String>] A list of station names.
+      def list = @list.map { _1.name.to_s }
 
-      def stations_on_start = 2
+      # Adds an intermediate station to the route.
+      #
+      # The station is added before the last station in the route.
+      #
+      # @param station [#name] An object that responds to `#name`.
+      # @return [Array<#name>] The updated list of stations.
+      def add(station) = @list.insert(-2, station)
+
+      # Removes an intermediate station from the route.
+      #
+      # The first and last stations cannot be removed.
+      #
+      # @param station [#name] An object that responds to `#name`.
+      # @return [#name, nil] The removed station, or `nil` if no station was removed.
+      def remove(station)
+        @list.delete(station) unless [@list.first, @list.last].include?(station)
+      end
     end
   end
 end
--- spec/lesson_4/lib/railroad/route_spec.rb
+++ spec/lesson_4/lib/railroad/route_spec.rb
@@ -7,38 +7,71 @@ class DummyStation
 end
 
 describe Lesson4::Railroad::Route do
-  let!(:route) { described_class.new(DummyStation.new('first'), DummyStation.new('last')) }
+  let(:first_station) { DummyStation.new('First') }
+  let(:last_station) { DummyStation.new('Last') }
+  let(:route) { described_class.new(first_station, last_station) }
 
-  context 'when no stations passed to a new route' do
-    it { expect { described_class.new }.to raise_error(/No stations/) }
-  end
+  describe '.new' do
+    context 'when no stations are provided' do
+      it 'raises a StationsNumberError' do
+        expect { described_class.new }.to raise_error(Lesson4::Railroad::Route::StationsNumberError, /No stations/)
+      end
+    end
 
-  context 'when created has first and last stations' do
-    it { expect(route).to respond_to(:list) }
-    it { expect(route).to respond_to(:add).with(1).argument }
-    it { expect(route).to respond_to(:remove).with(1).argument }
+    context 'when only one station is provided' do
+      it 'raises a StationsNumberError' do
+        expect { described_class.new(first_station) }.to raise_error(Lesson4::Railroad::Route::StationsNumberError, /Only one station/)
+      end
+    end
 
-    describe '#list' do
-      it { expect(route.list).to eq %w[first last] }
+    context 'when more than two stations are provided' do
+      it 'raises a StationsNumberError' do
+        extra_station = DummyStation.new('Extra')
+        expect { described_class.new(first_station, last_station, extra_station) }
+          .to raise_error(Lesson4::Railroad::Route::StationsNumberError, /3 stations given/)
+      end
     end
   end
 
-  context 'when add or remove stations' do
-    let(:other) { DummyStation.new('other') }
+  describe '#list' do
+    it 'returns the list of station names' do
+      expect(route.list).to eq %w[First Last]
+    end
+  end
 
-    before { route.add(other) }
+  describe '#add' do
+    let(:intermediate_station) { DummyStation.new('Intermediate') }
 
-    describe '#add' do
-      it { expect(route.list.count).to eq 3 }
-      it { expect(route.list[1]).to eq 'other' }
+    it 'adds an intermediate station to the route' do
+      route.add(intermediate_station)
+      expect(route.list).to eq %w[First Intermediate Last]
+    end
+  end
+
+  describe '#remove' do
+    let(:intermediate_station) { DummyStation.new('Intermediate') }
+
+    before { route.add(intermediate_station) }
+
+    context 'when removing an intermediate station' do
+      it 'removes the station from the route' do
+        route.remove(intermediate_station)
+        expect(route.list).to eq %w[First Last]
+      end
     end
 
-    describe '#remove' do
-      before { route.remove(other) }
+    context 'when trying to remove the first station' do
+      it 'does not remove the station' do
+        route.remove(first_station)
+        expect(route.list).to eq %w[First Intermediate Last]
+      end
+    end
 
-      it { expect(route.list.count).to eq 2 }
-      it { expect(route.list.first).to eq 'first' }
-      it { expect(route.list.last).to eq 'last' }
+    context 'when trying to remove the last station' do
+      it 'does not remove the station' do
+        route.remove(last_station)
+        expect(route.list).to eq %w[First Intermediate Last]
+      end
     end
   end
 end
